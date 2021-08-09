a6f584a5bb3fae75ec05f7f664ae06e4a352075a
GSaucedo <g.saucedo.kd@gmail.com>
2021-08-09 15:43:46 -0500

    Buttons adde on boilerplate

--- app.js
+++ app.js
@@ -25,14 +25,13 @@ function addBookToLibrary() {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
   populateHTML(newBook);
-  console.log(myLibrary);
   cancel();
 }
 
 function populateHTML(book) {
+  /*book__content*/
   const author = document.createElement("p");
   author.textContent = `Written by ${book.author}`;
-
   const pages = document.createElement("p");
   pages.textContent = `${book.pages} pages`;
 
@@ -53,20 +52,36 @@ function populateHTML(book) {
   checkBoxContainer.textContent = "Mark as read ";
   checkBoxContainer.appendChild(label);
 
-  const div = document.createElement("div");
-  div.classList.add("book__content");
-  div.appendChild(author);
-  div.appendChild(pages);
-  div.appendChild(checkBoxContainer);
+  const book__content = document.createElement("div");
+  book__content.classList.add("book__content");
+  book__content.appendChild(author);
+  book__content.appendChild(pages);
+  book__content.appendChild(checkBoxContainer);
+  /*end book__content
+  book__header*/
+  const book__header = document.createElement("h2");
+  book__header.classList.add("book__header");
+  book__header.textContent = book.title;
+  /*end book__header 
+  book__footer*/
+  const book__footer = document.createElement("div");
+  book__footer.classList.add("book__footer");
 
-  const header = document.createElement("h2");
-  header.classList.add("book__header");
-  header.textContent = book.title;
+  const btn_primary = document.createElement("button");
+  btn_primary.textContent = "Edit";
+  btn_primary.classList.add("book__footer--btn-primary");
+  const btn_secondary = document.createElement("button");
+  btn_secondary.textContent = "Remove";
+  btn_secondary.classList.add("book__footer--btn-secondary");
 
+  book__footer.appendChild(btn_primary);
+  book__footer.appendChild(btn_secondary);
+  /*end book__footer */
   const article = document.createElement("article");
   article.classList.add("book");
-  article.appendChild(header);
-  article.appendChild(div);
+  article.appendChild(book__header);
+  article.appendChild(book__content);
+  article.appendChild(book__footer);
 
   main.appendChild(article);
 }
